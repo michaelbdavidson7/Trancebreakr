5edcfbc641061b08974fffe1c3eed606af71c750
mike davidson <michaelbdavidson7@gmail.com>
2022-11-17 23:28:13 -0500

    removed the clickonce stuff and redid it with administrator within the app

--- Trancebreakr/MainWindow.xaml.cs
+++ Trancebreakr/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32.TaskScheduler;
 using Xceed;
+using System.Security.Principal;
+using System.Management;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Trancebreakr
 {
@@ -25,11 +29,43 @@ namespace Trancebreakr
         string mainProgramKey = "Trancebreakr";
         public MainWindow()
         {
+            if (!IsRunningAsAdministrator())
+            {
+                // Setting up start info of the new process of the same application
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(Assembly.GetEntryAssembly().CodeBase);
+
+                // Using operating shell and setting the ProcessStartInfo.Verb to “runas” will let it run as admin
+                processStartInfo.UseShellExecute = true;
+                processStartInfo.Verb = "runas";
+
+                // Start the application as new process
+                Process.Start(processStartInfo);
+
+                // Shut down the current (old) process
+                System.Windows.Application.Current.Shutdown();
+            }
+
             InitializeComponent();
             UpdateScheduledTasksList();
         }
 
-        private void SetScheduleButton_Click(object sender, RoutedEventArgs e)
+    /// <summary>
+    /// Function that check's if current user is in Aministrator role
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsRunningAsAdministrator()
+    {
+        // Get current Windows user
+        WindowsIdentity windowsIdentity = WindowsIdentity.GetCurrent();
+
+        // Get current Windows user principal
+        WindowsPrincipal windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+
+        // Return TRUE if user is in role "Administrator"
+        return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+    }
+
+    private void SetScheduleButton_Click(object sender, RoutedEventArgs e)
         {
             var timeOfDay = timeOfDayTextBox.Text;
             var numberOfMinutesWarning = numberOfMinutesWarningTextBox.Text;
